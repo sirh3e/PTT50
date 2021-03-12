e258432eefb8a99e089747c80cd5c7d0763059b7
sirh3e <sirh3e@protonmail.com>
2021-03-12 19:11:29 +0100

    [MODIFIED] PTT50/Ring.py refactored remove static ring file path use of config instand

--- PTT50/Ring.py
+++ PTT50/Ring.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+from config import Config
 from utilities import write_is_ringing
 
 def main():
-    write_is_ringing("/home/pi/TwinklePTT50/ring.txt")
+    write_is_ringing(Config.RING_FILE_PATH)
 
 if __name__ == '__main__':
     main()
