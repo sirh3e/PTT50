af2bee7370388170ea0f35c3506747eb8e4afd7e
sirh3e <sirh3e@protonmail.com>
2021-03-12 18:06:08 +0100

    [MODIFIED] PTT50/Ringing.py use of PTT50/timeout.py

--- PTT50/Ringing.py
+++ PTT50/Ringing.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 import time
 import os
 from pin import Pin
+from timeout import Timeout
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -23,14 +24,14 @@ while(True):
     f = open("/home/pi/TwinklePTT50/ring.txt", "r")
     y = (f.readline(1) == "1")
     f.close()
-    time.sleep(0.1)
+    time.sleep(Timeout.Short)
     GPIO.output(Pin.Ringing, 1)
 
     while(y):
         f = open("/home/pi/TwinklePTT50/ring.txt", "r")
         y = (f.readline(1) == "1")
         f.close()
-        time.sleep(0.1)
+        time.sleep(Timeout.Short)
         x += 1
         #print(x)
         if(x % (ring + pause) == ring):
